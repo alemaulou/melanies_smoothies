cc0dbe10856ccf1558d11db52916b7a21a303601
Alessandro Lou <alessandrolou@Alessandros-MacBook-Air.local>
2025-12-30 10:11:21 -0800

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,6 @@ if ingredients_list:
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.write(search_on)
-
         
         search_on = session.table("smoothies.public.fruit_options").filter(col('FRUIT_NAME') == fruit_chosen).select(col('SEARCH_ON')).collect()[0][0]
         
