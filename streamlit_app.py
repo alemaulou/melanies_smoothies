65ec16e3bac16f7750d3f44e72504555249690b9
Alessandro Lou <alessandrolou@Alessandros-MacBook-Air.local>
2025-12-30 10:20:01 -0800

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,8 @@ if ingredients_list:
         # search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-        my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+        search_on = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on) 
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
