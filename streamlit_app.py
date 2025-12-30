c7954ef4674df14d7e628fc198fdc69345fd201f
Alessandro Lou <alessandrolou@Alessandros-MacBook-Air.local>
2025-12-30 10:15:15 -0800

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from snowflake.snowpark.functions import col
 import requests
 import pandas
 
-
+st.title('test')
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
 st.write(
