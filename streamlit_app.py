b7604855dfb73a95c28fab608b0f05fed7c7f642
Alessandro Lou <alessandrolou@Alessandros-MacBook-Air.local>
2025-12-29 21:10:22 -0800

    most recent changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
 
 
 # Write directly to the app
@@ -49,6 +47,8 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
     st.write(my_insert_stmt)
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
     
     # st.write(my_insert_stmt)
     if time_to_insert:
